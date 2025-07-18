4299d8fd9fa8c54383a502bbf58869bd9e8f5e8b
sp0389 <seventh.theory@protonmail.com>
2025-07-18 15:25:55 +1000

    feat: add initial seed data

--- FinanceApi/Infrastructure/Persistence/ApplicationDbContext.cs
+++ FinanceApi/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -8,7 +8,6 @@ public class ApplicationDbContext : IdentityDbContext<User>
 {
     public DbSet<Transaction> Transactions { get; set; }
     public DbSet<BudgetCategory> BudgetCategories { get; set; }
-    public DbSet<User> Users { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -19,5 +18,6 @@ public class ApplicationDbContext : IdentityDbContext<User>
     {
         base.OnModelCreating(modelBuilder);
         _ = new EntityMapper(modelBuilder);
+        _ = new Seed(modelBuilder);
     }
 }
---
+++ FinanceApi/Infrastructure/Persistence/Seed.cs
@@ -0,0 +1,69 @@
+﻿using FinanceApi.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApi.Infrastructure.Persistence
+{
+    public class Seed
+    {
+        public Seed(ModelBuilder mb)
+        {
+            InitUsers(mb);
+            InitUserRoles(mb);
+            InitUserToRole(mb);
+        }
+
+        private static void InitUsers(ModelBuilder mb)
+        {
+            mb.Entity<User>().HasData(
+                new User
+                {
+                    Id = "ca32e0e5-46b8-4f44-9a97-0d685a2c54b2",
+                    UserName = "a@a.com",
+                    NormalizedUserName = "A@A.COM",
+                    Email = "a@a.com",
+                    EmailConfirmed = true,
+                    PasswordHash = "AQAAAAIAAYagAAAAEHsSevUsbVfCvzTrAPeOAJGAdLJXoClxNuG4OJyPozgYXexeGOqLXgnIxAZgTQTbfA==",
+                    SecurityStamp = "M67EBX32EPBJDLSU75U3EA5SFKIR7MDP",
+                    ConcurrencyStamp = "3e098325-ba04-4578-8bd8-231bbf8dde66",
+                    PhoneNumber = null,
+                    PhoneNumberConfirmed = false,
+                    TwoFactorEnabled = false,
+                    LockoutEnabled = false,
+                    AccessFailedCount = 0,
+                }
+            );
+        }
+
+        private static void InitUserRoles(ModelBuilder mb)
+        {
+            mb.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "ae18c1e2-1261-4c99-ace3-039873bb14f2",
+                    Name = "Member",
+                    NormalizedName = "MEMBER",
+                    ConcurrencyStamp = "fac56c34-35bd-49db-84b1-ea12e216462b",
+                },
+                new IdentityRole
+                {
+                    Id = "0933e74e-48a0-408e-8cf6-e33a4dbff132",
+                    Name = "Adminstrator",
+                    NormalizedName = "ADMINISTRATOR",
+                    ConcurrencyStamp = "d35caf6c-b859-4c1c-95c6-1611649fb285",
+                }
+            );
+        }
+
+        private static void InitUserToRole(ModelBuilder mb)
+        {
+            mb.Entity<IdentityUserRole<string>>().HasData(
+               new IdentityUserRole<string>
+               {
+                   RoleId = "0933e74e-48a0-408e-8cf6-e33a4dbff132",
+                   UserId = "ca32e0e5-46b8-4f44-9a97-0d685a2c54b2"
+               }
+            );
+        }
+    }
+}
